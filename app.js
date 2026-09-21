3681185feedc74eeb532f514e7b9e75f65c72684
唐润泽 <tangrunze.52712@bytedance.com>
2026-09-22 02:10:10 +0800

    Improve Huangpu River: soft scaling water band (dual-stroke)
    Change-Id: Ifca9db0e9361b72772546d52feb919714b1297bc

--- app.js
+++ app.js
@@ -132,9 +132,12 @@
     if(!gRiver) return;
     var pts=D.river.huangpu, dstr='';
     pts.forEach(function(pt,i){ var xy=project(pt[0],pt[1]); dstr+=(i===0?'M':'L')+xy[0].toFixed(1)+','+xy[1].toFixed(1); });
-    var path=document.createElementNS('http://www.w3.org/2000/svg','path');
-    path.setAttribute('d',dstr);
-    gRiver.appendChild(path);
+    // 双层描边：浅色外缘 + 深色主体，做出有宽度的自然江面（随缩放一起变宽）
+    var edge=document.createElementNS('http://www.w3.org/2000/svg','path');
+    edge.setAttribute('d',dstr); edge.setAttribute('class','river-edge');
+    var main=document.createElementNS('http://www.w3.org/2000/svg','path');
+    main.setAttribute('d',dstr); main.setAttribute('class','river-main');
+    gRiver.appendChild(edge); gRiver.appendChild(main);
   })();
 
   /* ---------- 3.5 参考图层：地铁主干线 + 商圈CBD ---------- */
